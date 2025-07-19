8db98e9439cec1eeec030bb835b120f562d37199
azhukau-dev <azhukaudev@gmail.com>
2025-07-20 01:53:28 +0200

    feat: rework todo app example

--- apps/client/src/app/pages/todo-list/todo-list.ts
+++ apps/client/src/app/pages/todo-list/todo-list.ts
@@ -1,12 +1,22 @@
 import { ChangeDetectionStrategy, Component, model } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { injectAction, injectMutation, injectQuery } from 'convex-angular';
+import { ButtonModule } from 'primeng/button';
+import { CheckboxModule } from 'primeng/checkbox';
+import { InputTextModule } from 'primeng/inputtext';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
 
 import { api } from '../../../convex/_generated/api';
 import { Id } from '../../../convex/_generated/dataModel';
 
 @Component({
-  imports: [FormsModule],
+  imports: [
+    FormsModule,
+    ButtonModule,
+    CheckboxModule,
+    InputTextModule,
+    ProgressSpinnerModule,
+  ],
   selector: 'cva-todo-list',
   templateUrl: 'todo-list.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -15,14 +25,19 @@ import { Id } from '../../../convex/_generated/dataModel';
   },
 })
 export default class TodoList {
-  readonly count = model(3);
+  readonly newTask = model('');
+  readonly count = model(20);
 
   readonly todos = injectQuery(api.todos.listTodos, () => ({
     count: this.count(),
   }));
 
+  readonly addTodo = injectMutation(api.todos.addTodo, {
+    onSuccess: () => this.newTask.set(''),
+  });
   readonly completeTodo = injectMutation(api.todos.completeTodo);
   readonly reopenTodo = injectMutation(api.todos.reopenTodo);
+  readonly deleteTodo = injectMutation(api.todos.deleteTodo);
 
   readonly completeAll = injectAction(api.todoFunctions.completeAllTodos);
   readonly reopenAll = injectAction(api.todoFunctions.reopenAllTodos);
@@ -33,6 +48,14 @@ export default class TodoList {
     } else {
       this.completeTodo.mutate({ id });
     }
+  }
+
+  handleAddTodo() {
+    this.addTodo.mutate({ title: this.newTask() });
+  }
+
+  handleDeleteTodo(id: Id<'todos'>) {
+    this.deleteTodo.mutate({ id });
   }
 
   handleCompleteAll() {
